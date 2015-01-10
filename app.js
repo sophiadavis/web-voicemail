9e1bcd5154151830ac898750bbbffb39d0315f7a
Sophia Davis <scdgrapefruit@gmail.com>
2015-01-10 17:43:49 +0100

    Use basic jade template to make flash work

--- app.js
+++ app.js
@@ -11,17 +11,18 @@ var session = require('express-session');
 
 var basic = auth.basic({
     realm: "Simon Area.", // wtf
-    file: __dirname + "/passwds.txt"  // gevorg:gpass, Sarah:testpass ...
+    file: __dirname + "/passwds.txt"
 });
 
 var app = express();
+
+app.set('view engine', 'jade');
+
 app.use(auth.connect(basic));
 app.use(busboy());
 app.use(cookieParser('secret'));
-app.use(require('express-jquery')('/jquery.js'));
 app.use(session({
             secret: "cookie_secret",
-            // cookie: { maxAge: 60000 },
             resave: false,
             saveUninitialized: false}));
             app.use(flash());
@@ -31,24 +32,23 @@ app.post('/', function(req, res) {
     req.pipe(req.busboy);
     req.busboy.on('file', function (fieldname, file, filename) {
 
-        //Path where image will be uploaded
         fstream = fs.createWriteStream(__dirname + '/uploads/' + filename);
         file.pipe(fstream);
         fstream.on('close', function () {
             console.log("Finished uploading " + filename);
             req.flash('info', 'it worked');
-            console.log(req.flash('info'));
-            // var str = $("div#the-flash-box").text();
-            console.log(str);
-            res.redirect('back');
-            // res.render('index', { messages: req.flash('info') });
+            res.redirect('/');
         });
     });
 });
 
-app.get('/', function(req, res) {
-    var index = path.join(__dirname, 'public', 'index.html');
-    res.sendFile(index);
+app.get('/', function (req, res) {
+    res.render('index', {message: req.flash('info')});
+})
+
+app.get('/client.js', function(req, res) {
+    var clientjs = path.join(__dirname, 'public', 'client.js');
+    res.sendFile(clientjs);
 });
 
 
